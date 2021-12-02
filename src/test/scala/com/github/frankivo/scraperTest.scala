6e05c6b80083331dd024ce1bf8f0a7d4df2ef432
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-02 13:29:52 +0100

    fetch from disk once

--- src/test/scala/com/github/frankivo/scraperTest.scala
+++ src/test/scala/com/github/frankivo/scraperTest.scala
@@ -6,40 +6,35 @@ import scala.io.Source
 
 class scraperTest extends AnyFunSuite :
 
+  lazy val html: String = Source.fromResource("events.html").getLines().mkString("\n")
+
   test("getBody results in fewer lines") {
-    val start = getTestHtml.split("\n").length
-    val body = scraper.getBody(getTestHtml)
-    val parsed = scraper.getBody(getTestHtml).split("\n").length
+    val start = html.split("\n").length
+    val body = scraper.getBody(html)
+    val parsed = scraper.getBody(html).split("\n").length
 
     assert(parsed < start)
   }
 
   test("xml is valid") {
-    val body = scraper.getBody(getTestHtml)
+    val body = scraper.getBody(html)
     val xml = scraper.toXML(body)
     assert(xml.getClass == classOf[scala.xml.Elem])
     assert(xml.toString().startsWith("<main"))
   }
 
   test("amount of events is 12") {
-    val body = scraper.getBody(getTestHtml)
+    val body = scraper.getBody(html)
     val xml = scraper.toXML(body)
     val events = scraper.getEvents(xml)
     assert(events.length == 12)
   }
 
   test("first event is 'Street Stocks and MX-5s at Road America'") {
-    val body = scraper.getBody(getTestHtml)
+    val body = scraper.getBody(html)
     val xml = scraper.toXML(body)
     val events = scraper.getEvents(xml)
     assert(events.head.name.equals("Street Stocks and MX-5s at Road America"))
-  }
-
-  def getTestHtml: String = {
-    Source
-      .fromResource("events.html")
-      .getLines()
-      .mkString("\n")
   }
 
 end scraperTest
