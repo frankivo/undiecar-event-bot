4bc9208b067436e7f9051658110c25aa3ee0c780
Frank Oosterhuis <frank+github@scriptzone.nl>
2021-12-06 15:30:08 +0100

    Randomize test

--- src/test/scala/com/github/frankivo/scraperTest.scala
+++ src/test/scala/com/github/frankivo/scraperTest.scala
@@ -4,11 +4,12 @@ import org.scalatest.funsuite.AnyFunSuite
 
 import java.time.LocalDate
 import scala.io.Source
+import scala.util.Random
 
 class scraperTest extends AnyFunSuite :
 
   lazy val html: String = Source.fromResource("events.html").getLines().mkString("\n")
-  lazy val events : Seq[event] = scraper.getEvents(scraper.toXML(scraper.getBody(html)))
+  lazy val events: Seq[event] = scraper.getEvents(scraper.toXML(scraper.getBody(html)))
 
   test("body has correct amount of lines") {
     val body = scraper.getBody(html)
@@ -32,8 +33,10 @@ class scraperTest extends AnyFunSuite :
   }
 
   test("events have correct date") {
-    val exp = Seq("2021-11-30", "2021-12-07", "2021-12-14", "2021-12-21", "2021-12-28", "2022-01-04", "2022-01-11", "2022-01-18", "2022-01-25", "2022-02-08", "2022-02-15", "2022-02-22")
-      .map(LocalDate.parse)
+    val exp = Random.shuffle(
+      Seq("2021-11-30", "2021-12-07", "2021-12-14", "2021-12-21", "2021-12-28", "2022-01-04",
+        "2022-01-11", "2022-01-18", "2022-01-25", "2022-02-08", "2022-02-15", "2022-02-22")
+    ).map(LocalDate.parse)
     val act = events.map(_.date)
 
     assert(act.length == 12)
